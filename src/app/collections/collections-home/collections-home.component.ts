ab9da508563eac2d80eae496ebec2bc7810c133a
tsmithe2 <thomas_smithey@yahoo.com>
2022-11-10 18:47:09 -0600

    new app

---
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-home',
+  templateUrl: './collections-home.component.html',
+  styleUrls: ['./collections-home.component.css']
+})
+export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Designer', employed: true },
+    { name: 'Jill', age: 26, job: 'Engineer', employed: false},
+    { name: 'Elyse', age: 25, job: 'Enginer', employed: true }
+  ];
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job'},
+    { key: 'employed', label: 'Employed'}
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
